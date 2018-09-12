44c73a31f9cecb0a2b606c4e7638cf9c3e136522
Efua <kagyarek@u.rochester.edu>
2018-09-12 16:37:59 -0400

    efua's test file
    something i created just to see how this works out. my php file from the last project.

---
+++ tab-1-intro.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Efua Agyare-Kumi, Lab 01 CSC 174</title>
+		<?php include "inc/html-top.inc"; ?>
+		<!--got an error from this but not sure what to replace the | sign with?-->
+	</head> 
+	
+	<body class="grid-wrapper">
+		<h1 class="item1">Efua.</h1>
+		<div class="item2">
+			<?php include "inc/nav.inc"; ?>
+		</div>
+		<h2 class="item3">Storyteller. Community builder. Pan-Africanist.</h2>
+		<figure class="item4">
+		<img class="title-photo" src="images/me.jpg" alt="picture of me in South Africa">
+		<figcaption>South Africa, 2018</figcaption>
+		</figure>
+		<article class="item5">
+			<p>These words describe some of the most important aspects of my being. One word that was up there for a long time and recently fell away was medicine junkie. One word that will be up there soon is changemaker. The ones you see -- and even the ones you don’t -- are all connected:  I tell stories and build communities in order to inspire change, to spark a roaring fire in others. I do not always do this within Africa, but my heart is always calling for the continent, which I consider my home. If this sounds grand, lofty or pretentious, perhaps a glimpse into my background will help.</p>
+			<a href="tab-2-the-beginning.html">Click next</a>
+		</article>
+	<?php include "inc/scripts.inc"; ?>
+	</body>
+</html>
+
+
+
+
+
+
