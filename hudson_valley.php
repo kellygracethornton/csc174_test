9d2fe84c9fff08d02e5ce36c14c986b332fee04d
Kelly <kellygracethornton@gmail.com>
2018-09-12 16:42:16 -0400

    kt_page
    php page

---
+++ hudson_valley.php
@@ -0,0 +1,25 @@
+<?php include "inc/html-top.inc";?>
+
+<body>
+
+	<div class="container">
+		<?php include "inc/header.inc";?>
+
+
+			<?php include "inc/nav.inc";?>
+
+			<img src="images/graduation.jpg" alt="kelly graduates at hudsonvalley" class="falls">
+
+			<p>January of 2015 I entered Hudson Valley Community College. I had started their digital media associates program. The program is only about five years old so it is a new subject. This major was not available when I initially graduated high school in 2010. I was able to build my resume with skill and was able to build a portfolio. This made me realize my love for web art, and digital art. I graduated with honors and was on the President's list the whole time I was a student there. Learning that art was the best way to help Deaf community I found my passion within the digital world to convey the messages that I thinks need to be spoken about. I focused on social issues that we all face hearing or deaf. Then I emulated a few artists but using Deaf world perspectives. Then towards the end of my time at Hudson Valley I focused more so on the form and aesethic of sign language. Also at the end I knew I wanted to pursue more of my education.</p>
+
+			<?php include "inc/footer.inc";?>
+
+
+	</div>
+
+	
+
+<script src="js/menu-highlighter.js"></script>
+</body>
+
+</html>
